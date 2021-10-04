120c4b0a57e825d9a9eb465dfff1336cb03d2fd2
Rafael Casachi <rcasachi@gmail.com>
2021-10-04 18:22:42 -0300

    feat(locales): add general locales in core

---
+++ src/core/locales/exception.locales.ts
@@ -0,0 +1,6 @@
+export const ERROR_500 = 'Internal Server Error';
+export const ERROR_400 = 'Bad Request';
+export const ERROR_404 = 'Not Found';
+
+export const STATUS_FAIL = 'fail';
+export const STATUS_ERROR = 'error';
--- src/core/locales/response.locales.ts
+++ src/core/locales/response.locales.ts
@@ -1,1 +1,5 @@
 export const STATUS_ACTIVE = 'active';
+
+export const localeIsRequired = (field: string) => `${field} is required`;
+export const localeNotFoundRoute = (originalUrl: string) =>
+  `There is no ${originalUrl} on this server`;
