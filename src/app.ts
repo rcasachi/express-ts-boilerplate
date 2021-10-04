984dea3523584aae13e382d2698056e1c7b99f4c
Rafael Casachi <rcasachi@gmail.com>
2021-10-04 16:40:54 -0300

    fix(app): change app configuration

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,19 @@ import 'reflect-metadata';
 import dotenv from 'dotenv';
 import express from 'express';
 
+import { AppModule } from './app.module';
+import { useModuleRoutes } from './core/package/module/module.package';
 import connection from './database/connection';
 import exceptions from './exceptions';
 import middlewares from './middlewares';
 import notFound from './middlewares/not-found';
-import routes from './routes';
 
 dotenv.config();
 const app = express();
 
 connection();
 middlewares(app);
-app.use(routes);
+useModuleRoutes(AppModule, app);
 app.all('*', notFound);
 app.use(exceptions);
 
